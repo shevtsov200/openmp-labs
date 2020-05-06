de3300cf8b1de97d7fd1b44af0738a3c92014c45
Daniil Shevtsov <me@daniilshevtsov.com>
2020-05-06 10:05:28 +0300

    get matrix without given row and column

--- lab7_2/lab7_2.c
+++ lab7_2/lab7_2.c
@@ -1,3 +1,32 @@
+void getCofactor(int mat[N][N], int n, int p, int q, int result[N][N]) 
+{ 
+    int i = 0, j = 0; 
+
+    // Looping for each element of the matrix 
+    for (int row = 0; row < n; row++) 
+    { 
+        for (int col = 0; col < n; col++) 
+        { 
+            //  Copying into temporary matrix only those element 
+            //  which are not in given row and column 
+            if (row != p && col != q) 
+            { 
+                result[i][j++] = mat[row][col]; 
+
+                // Row is filled, so increase row index and 
+                // reset col index 
+                if (j == n - 1) 
+                { 
+                    j = 0; 
+                    i++; 
+                } 
+            } 
+        } 
+    } 
+}
+
+
+
 int main(int argc, char **argv) {
     return 0;
 }
